b1f446c78cc56877a92a9e1ba29463ebe30d7b58
Whitney Champion <whitney.ellis.champion@gmail.com>
2021-07-08 14:05:03 -0400

    Create watch-plaso-to-s3.sh

---
+++ bash/watch-plaso-to-s3.sh
@@ -0,0 +1,15 @@
+# Watch this directory for new Plaso files (ie: vr_kapetriage_$system.plaso)
+
+PARENT_DATA_DIR="/opt/Plaso_data"
+BUCKET_NAME = ""
+
+inotifywait -m -r -e close_write "$PARENT_DATA_DIR" --format "%f" | while read PLASO
+do
+  extension="${PLASO##*.}"
+  if [[ $extension == "plaso" ]]; then
+    # Get name of directory
+    echo $PLASO
+    # upload to S3
+    aws s3 cp $PARENT_DATA_DIR/$PLASO s3://$BUCKET_NAME/$PLASO
+  fi
+done
