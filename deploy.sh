943be9ebc900b042643f70008a049131051a06dc
Whitney Champion <whitney.ellis.champion@gmail.com>
2021-07-08 14:31:27 -0400

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+# Install requirements
+pip3 install boto3 pytz 
+
+# Copy files
+cp python/watch-s3-to-timesketch.py /opt/watch-s3-to-timesketch.py
+cp bash/watch-to-timesketch.sh /opt/watch-to-timesketch.sh
+cp bash/watch-plaso-to-s3.sh /opt/watch-plaso-to-s3.sh
+
+# Fix permissions
+chmod +x /opt/watch-plaso-to-s3.sh
+chmod +x /opt/watch-to-timesketch.sh
+
+# Configure services
+cp systemd/data-to-timesketch.service /etc/systemd/system/data-to-timesketch.service
+systemctl enable data-to-timesketch.service
+systemctl start data-to-timesketch.service
+
+cp systemd/watch-plaso-to-s3.servicee /etc/systemd/system/watch-plaso-to-s3.service
+systemctl enable watch-plaso-to-s3.service
+systemctl start watch-plaso-to-s3.service
+
+cp systemd/watch-s3-to-timesketch.service /etc/systemd/system/watch-s3-to-timesketch.service
+systemctl enable watch-s3-to-timesketch.service
+systemctl start watch-s3-to-timesketch.service
+
